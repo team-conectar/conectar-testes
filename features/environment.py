f025bad58a3da4ccf6f65dacdd4b9fab0cc18669
tekpixo <lincoln.mcosta@gmail.com>
2021-04-13 15:27:30 -0300

    Configuração do ambiente para deploy.

--- features/environment.py
+++ features/environment.py
@@ -1,9 +1,14 @@
 from selenium import webdriver
+import os
 
 
 def before_all(context):
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--no-sandbox")
     context.web = webdriver.Chrome(
-        executable_path='dependency/chromedriver.exe')    
+        executable_path='dependency/chromedriver.exe', chrome_options=chrome_options)
 
 
 def after_step(context, step):
